dd5ccd0131205900db0f7219454e12224a0bf005
方京超 <fangjingchao@fangjingchaodeMacBook-Pro.local>
2022-07-03 21:43:38 -0700

    baseline condition

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+var Config = {
+  apiKey: "AIzaSyBO3jIJaIbWQSIpjKHfx5kfgnunHhPhE2k",
+  authDomain: "edulivechat.firebaseapp.com",
+  //databaseURL: "https://edulivestream-dummy-default-rtdb.firebaseio.com",
+  projectId: "edulivechat",
+  storageBucket: "edulivechat.appspot.com",
+  messagingSenderId: "38433955037",
+  appId: "1:38433955037:web:fc3ec0d501792a0aa10f34",
+  measurementId: "G-NSMZK0RJD5"
+};
+firebase.initializeApp(Config)
